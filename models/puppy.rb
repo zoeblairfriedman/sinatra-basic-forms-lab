2d8c119d7b76938f3061d1d0f449bb561b77a231
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-02-23 18:25:41 -0500

    commit tree 5532ea66e0c4420b293ed7a46fe7728491ddcbde

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,1 +1,12 @@
-# since we are not using ActiveRecord in this lab, you write a bare Ruby model (like you did in MOD1)
+class Puppy
+attr_accessor :name, :breed, :age
+
+def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+end
+
+
+
+end
